d9120f8a0af9052e617654874f1e422017181716
bdub <wpwarman@gmail.com>
2022-07-22 11:52:32 -0700

    submitted solution

--- GenerateDocument.py
+++ GenerateDocument.py
@@ -1,10 +1,17 @@
 print("Generate Document")
 
-def generateDocument(characters, document):
+def generateDocument(characters, document):	
+	for x in document:
+		if x in characters:
+			print(f"I found {x}")
+			characters = characters.replace(x, '', 1)
+			print(characters)
+		else:
+			return False
 	return True
 
-characters = "ldsjdowmcONJjpk"
-document = "Blue Marlin - Ibiza"
+characters = "abcabc"
+document = "aabbcc"
 
 print(f"The characters are: {characters}")
 print(f"The document requested is: {document}")
