94c422005e45e5fb797dc3a5f29fcf4b7b5ce3d8
bdub <wpwarman@gmail.com>
2022-07-22 11:10:12 -0700

    Basic framework, no solution

---
+++ GenerateDocument.py
@@ -0,0 +1,15 @@
+print("Generate Document")
+
+def generateDocument(characters, document):
+	return True
+
+characters = "ldsjdowmcONJjpk"
+document = "Blue Marlin - Ibiza"
+
+print(f"The characters are: {characters}")
+print(f"The document requested is: {document}")
+	 
+if generateDocument(characters, document):
+    print("The document CAN be generated")
+else:
+    print("The document CANNOT be generated")
