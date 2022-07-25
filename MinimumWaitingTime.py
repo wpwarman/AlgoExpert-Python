1e6aa75e66b7b97e6e5c4b693a9db0f4876dc4ea
bdub <wpwarman@gmail.com>
2022-07-25 13:19:28 -0700

    basic framework - no solution

---
+++ MinimumWaitingTime.py
@@ -0,0 +1,9 @@
+print("Minimum Waiting Time")
+
+def minimumWaitingTime(queries):
+	return 0
+
+executionTimes = [3,2,1,2,6]
+print(f"The execution times are: {executionTimes}")
+minWaitTime = minimumWaitingTime(executionTimes)
+print(f"The minimum wait time is: {minWaitTime}")
