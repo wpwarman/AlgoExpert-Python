5079d12caffff3fcc5049cf2f0d051c8f92c1d49
bdub <wpwarman@gmail.com>
2022-07-25 14:32:50 -0700

    basic framework - no solution

---
+++ BubbleSort.py
@@ -0,0 +1,11 @@
+print("Bubble Sort")
+
+def bubbleSort(array):
+	n = len(array)
+	return n
+
+sortMe = [8, 5, 2, 9, 5, 6, 3]
+
+print(f"The unsorted array is: {sortMe}")
+sorted = bubbleSort(sortMe)
+print(f"The sorted array is: {sorted}")
