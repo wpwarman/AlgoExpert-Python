f0407b9e8a1be5bf561bb3c4ca9adefb162bad35
bdub <wpwarman@gmail.com>
2022-07-25 15:25:21 -0700

    basic framework - no solution

---
+++ SelectionSort.py
@@ -0,0 +1,10 @@
+print("Selection Sort")
+
+def selectionSort(array):
+	return array
+
+sortMe = [8, 5, 2, 9, 5, 6, 3]
+
+print(f"The unsorted array is: {sortMe}")
+sorted = selectionSort(sortMe)
+print(f"The sorted array is: {sorted}")
