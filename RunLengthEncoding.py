24d0ac12645df72104bd819efdea998ed391b592
bdub <wpwarman@gmail.com>
2022-07-25 11:35:08 -0700

    This version uses a List

--- RunLengthEncoding.py
+++ RunLengthEncoding.py
@@ -1,26 +1,26 @@
 print("Run Length Encoding")
 
-def runLengthEncoding(string):
+def runLengthEncoding(string):	
+	message = list(string)
+
 	encodedText = []
-	count = 0
-	value = string[0]
-	print(f"encodedText: {encodedText} / count: {count} / string {string}")
-	while count < 9 and len(string) > 0:
-		print("-- in while") 
-		if string[0] == string[count]:
-			print("-- in if")
-			count = count + 1		
-		else:
-			print("-- in else")
-			encodedText.append(count)
-			encodedText.append(value)
-			string = string.replace(value, "", count)
-			print (f"The shortened string is: {string}")
-		#runLengthEncoding(string)
-	return encodedText
+	
+	while len(message) > 0:
+		count = 0
+		value = message[0]
+		while count < 9 and count < len(message) and message[count] == message[0]:
+			print(f"count: {count} / value: {value} / message[value]: {message[count]}")	
+			count = count + 1
+		encodedText.append(count)
+		encodedText.append(value)
+		del message[:count]
+		print(f"message: {message}")
+		print(f"encodedText: {encodedText}")
+		text = "".join(map(str,encodedText))
+	return text
+
 
 message = "AAAAAAAAAAAAABBCCCCDD"
-
 print(f"The message is: {message}")
 encodedText = runLengthEncoding(message)
 print(f"The cipher text is: {encodedText}")
