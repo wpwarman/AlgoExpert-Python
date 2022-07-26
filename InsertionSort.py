322821b497290c935c1364975b2878900528373d
bdub <wpwarman@gmail.com>
2022-07-25 17:37:39 -0700

    basic framework - no solution

---
+++ InsertionSort.py
@@ -0,0 +1,9 @@
+print("Insertion Sort")
+
+def insertionSort(array):
+	return array
+
+sortMe = [8, 5, 2, 9, 5, 6, 3]
+print(f"The unsorted array is: {sortMe}")
+sorted = insertionSort(sortMe)
+print(f"The sorted array is: {sorted}")
