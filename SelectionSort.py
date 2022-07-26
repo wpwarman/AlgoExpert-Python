58990bd04782faa41df2553fd3b4fd62c2e68118
bdub <wpwarman@gmail.com>
2022-07-25 17:28:30 -0700

    submitted solution

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,6 +1,19 @@
 print("Selection Sort")
 
 def selectionSort(array):
+	for bottomIndex in range(0,len(array)):
+		lowestValue = array[bottomIndex]
+		lowestIndex = bottomIndex
+		
+		for index in range(bottomIndex, len(array)):
+			x = array[index]			
+			if x < lowestValue:
+				lowestValue = x
+				lowestIndex = index
+			
+		swapValue = array[bottomIndex]
+		array[bottomIndex] = lowestValue
+		array[lowestIndex] = swapValue
 	return array
 
 sortMe = [8, 5, 2, 9, 5, 6, 3]
