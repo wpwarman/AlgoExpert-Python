cad87812c7a3d0dc3d9b4ff3e2a9971d92bc20a7
bdub <wpwarman@gmail.com>
2022-08-08 16:54:41 -0700

    submitted solution

--- Level2_AlgoExpert/SmallestDifference-1.py
+++ Level2_AlgoExpert/SmallestDifference-1.py
@@ -1,13 +1,16 @@
 print("Smallest Difference")
 
 def smallestDifference(arrayOne, arrayTwo):
-    smallestDelta = 100
+    #array1.sort(reverse=True)
+    #array2.sort(reverse=True)
+    #smallestDelta = array1[0] + array2[0] + 1
+    smallestDelta = -1
     smallestDeltaValues = []
     for index1, value1 in enumerate(arrayOne):
         print(f"array one - index {index1} | value: {value1}")
         for index2, value2 in enumerate(arrayTwo):
             delta = abs(value1 - value2)
-            if delta <= smallestDelta:
+            if delta <= smallestDelta or smallestDelta == -1:
                 smallestDelta = delta
                 smallestDeltaValues = [value1, value2]
             print(f"\tdifferences: a1[{index1}]/a2[{index2}] | {value1}/{value2} | abs diff {delta}")
